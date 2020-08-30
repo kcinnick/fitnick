b2b67c5528bf9b29e35745bbe4cf6be797b5f334
unknown <nicktucker4@gmail.com>
2020-08-30 13:56:12 -0400

    Cleaned up tests by collapsing some variables.

--- tests/test_fitnick.py
+++ tests/test_fitnick.py
@@ -20,6 +20,13 @@ HEART_DATERANGE_EXPECTED_ROWS = [
             ('Peak', datetime.date(2020, 8, 20), datetime.date(2020, 8, 27), Decimal('0.00000'), Decimal('0.00000'))
 ]
 
+HEART_PERIOD_EXPECTED_ROWS = [
+    ('Out of Range', Decimal('1297.00000'), datetime.date(2020, 8, 26), Decimal('2444.62820')),
+    ('Fat Burn', Decimal('110.00000'), datetime.date(2020, 8, 26), Decimal('814.44840')),
+    ('Cardio', Decimal('0.00000'), datetime.date(2020, 8, 26), Decimal('0.00000')),
+    ('Peak', Decimal('0.00000'), datetime.date(2020, 8, 26), Decimal('0.00000'))
+]
+
 
 def purge(db_connection, delete_sql_string, select_sql_string):
     """
@@ -37,27 +44,31 @@ def test_get_authorized_client():
     assert type(fitnick.get_authorized_client()) == fitbit.Fitbit
 
 
-def test_get_heart_rate_time_series_period():
+def test_get_heart_rate_time_series_period(date='2020-08-26'):
     db_connection = create_engine(f"postgres+psycopg2://{os.environ['POSTGRES_USERNAME']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_IP']}:5432/fitbit_test")
     authorized_client = fitnick.get_authorized_client()
-    delete_sql_string = f"delete from heart.daily where date='2020-08-26'"
-    select_sql_string = f"select * from heart.daily where date='2020-08-26'"
+
+    delete_sql_string = f"delete from heart.daily where date={date}"
+    select_sql_string = f"select * from heart.daily where date={date}"
 
     purge(db_connection, delete_sql_string, select_sql_string)
 
-    fitnick.get_heart_rate_time_series_period(authorized_client, db_connection=db_connection, date='2020-08-26', period='1d')
+    fitnick.get_heart_rate_time_series_period(
+        authorized_client,
+        db_connection=db_connection,
+        date={date},
+        period='1d'
+    )
+
     with db_connection.connect() as connection:
         # re-adding them
         rows = [i for i in connection.execute(select_sql_string)]
-        assert len(rows) == 4
+        assert sorted(rows) == sorted(HEART_PERIOD_EXPECTED_ROWS)
 
 
-def test_get_heart_rate_time_series_daterange():
+def test_get_heart_rate_time_series_daterange(base_date='2020-08-20', end_date='2020-08-27'):
     db_connection = create_engine(f"postgres+psycopg2://{os.environ['POSTGRES_USERNAME']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_IP']}:5432/fitbit_test")
     authorized_client = fitnick.get_authorized_client()
-
-    base_date = '2020-08-20'
-    end_date = '2020-08-27'
 
     delete_sql_string = f"delete from heart.daterange where base_date='{base_date}' and end_date='{end_date}'"
     select_sql_string = f"select * from heart.daterange where base_date='{base_date}' and end_date='{end_date}'"
