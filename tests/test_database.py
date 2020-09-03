8a03d8131b14b1641ec528a351884f96ba97708d
unknown <nicktucker4@gmail.com>
2020-09-03 15:19:47 -0400

    static-ized dates in test_compare_1d, added test for duplicates.

--- tests/test_database.py
+++ tests/test_database.py
@@ -9,14 +9,15 @@ from fitnick.models import heart_daily_table
 
 def test_compare_1d_heart_rate_zone_data():
     db_connection = create_db_engine(database='fitbit_test')
-    db_connection.execute(heart_daily_table.delete().where(heart_daily_table.columns.date == date.today().strftime('%Y-%m-%d')))
+    db_connection.execute(
+        heart_daily_table.delete().where(heart_daily_table.columns.date == date.today().strftime('2020-09-02')))
 
     get_heart_rate_zone_time_series(
         authorized_client=get_authorized_client(),
         database='fitbit_test',
         table=heart_daily_table,
         config={
-            'base_date': date.today().strftime('%Y-%m-%d'),
+            'base_date': '2020-09-02',
             'period': '1d'
         }
     )
@@ -25,3 +26,23 @@ def test_compare_1d_heart_rate_zone_data():
     )
     assert (heart_rate_zone, type(minutes_in_zone_today), type(minutes_in_zone_yesterday)) == (
         'Cardio', decimal.Decimal, decimal.Decimal)
+
+
+def test_check_for_duplicates():
+    """
+    Useful for asserting that there aren't duplicates in the database, which *should* be avoided in the code.
+    """
+    get_heart_rate_zone_time_series(
+        authorized_client=get_authorized_client(),
+        database='fitbit_test',
+        table=heart_daily_table,
+        config={
+            'base_date': date.today().strftime('2020-09-02'),
+            'period': '1d'
+        })
+    db_connection = create_db_engine(database='fitbit_test')
+    results = db_connection.execute(
+        heart_daily_table.select().where(heart_daily_table.columns.date ==
+                                         date.today().strftime('2020-09-02'))
+    )
+    assert len([result for result in results]) == 4
