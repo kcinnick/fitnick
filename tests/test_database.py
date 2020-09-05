3672a67a1e2c95cc397a87efd8d5423649d445ec
unknown <nicktucker4@gmail.com>
2020-09-05 00:15:23 -0400

    Reworked tests to match heart_rate.py change

--- tests/test_database.py
+++ tests/test_database.py
@@ -4,7 +4,7 @@ from datetime import date
 from fitnick.base.base import get_authorized_client, create_db_engine
 from fitnick.database.database import compare_1d_heart_rate_zone_data
 from fitnick.heart_rate.heart_rate import get_heart_rate_zone_time_series
-from fitnick.models import heart_daily_table
+from fitnick.heart_rate.models import heart_daily_table
 
 
 def test_compare_1d_heart_rate_zone_data():
@@ -14,8 +14,7 @@ def test_compare_1d_heart_rate_zone_data():
 
     get_heart_rate_zone_time_series(
         authorized_client=get_authorized_client(),
-        database='fitbit_test',
-        table=heart_daily_table,
+        engine=db_connection,
         config={
             'base_date': '2020-09-02',
             'period': '1d'
@@ -32,15 +31,15 @@ def test_check_for_duplicates():
     """
     Useful for asserting that there aren't duplicates in the database, which *should* be avoided in the code.
     """
+    db_connection = create_db_engine(database='fitbit_test')
+
     get_heart_rate_zone_time_series(
         authorized_client=get_authorized_client(),
-        database='fitbit_test',
-        table=heart_daily_table,
+        engine=db_connection,
         config={
             'base_date': date.today().strftime('2020-09-02'),
             'period': '1d'
         })
-    db_connection = create_db_engine(database='fitbit_test')
     results = db_connection.execute(
         heart_daily_table.select().where(heart_daily_table.columns.date ==
                                          date.today().strftime('2020-09-02'))
