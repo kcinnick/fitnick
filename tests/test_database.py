6e0b4a9c4e44895c61a4a7b4ff323bc706481a41
unknown <nicktucker4@gmail.com>
2020-09-06 03:06:30 -0400

    Updated tests to match heart_rate.py overhaul.

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,7 +3,7 @@ from datetime import date, timedelta
 
 from fitnick.base.base import get_authorized_client, create_db_engine
 from fitnick.database.database import compare_1d_heart_rate_zone_data
-from fitnick.heart_rate.heart_rate import get_heart_rate_zone_time_series
+from fitnick.heart_rate.heart_rate import insert_heart_rate_time_series_data
 from fitnick.heart_rate.models import heart_daily_table
 
 
@@ -15,17 +15,16 @@ def test_compare_1d_heart_rate_zone_data():
     db_connection.execute(
         heart_daily_table.delete().where(heart_daily_table.columns.date == today.strftime('%Y-%m-%d')))
 
-    get_heart_rate_zone_time_series(
-        authorized_client=get_authorized_client(),
-        engine=db_connection,
+    insert_heart_rate_time_series_data(
         config={
             'base_date': today.strftime('%Y-%m-%d'),
-            'end_date': yesterday.strftime('%Y-%m-%d')
+            'end_date': yesterday.strftime('%Y-%m-%d'),
+            'database': 'fitbit_test'
         }
     )
     heart_rate_zone, minutes_in_zone_today, minutes_in_zone_yesterday = compare_1d_heart_rate_zone_data(
-        database='fitbit_test', table=heart_daily_table
-    )
+        database='fitbit_test', table=heart_daily_table)
+
     assert (heart_rate_zone, type(minutes_in_zone_today), type(minutes_in_zone_yesterday)) == (
         'Cardio', decimal.Decimal, decimal.Decimal)
 
@@ -36,15 +35,16 @@ def test_check_for_duplicates():
     """
     db_connection = create_db_engine(database='fitbit_test')
 
-    get_heart_rate_zone_time_series(
-        authorized_client=get_authorized_client(),
-        engine=db_connection,
+    insert_heart_rate_time_series_data(
         config={
             'base_date': date.today().strftime('2020-09-02'),
-            'period': '1d'
-        })
+            'period': '1d',
+            'database': 'fitbit_test'}
+    )
+
     results = db_connection.execute(
         heart_daily_table.select().where(heart_daily_table.columns.date ==
                                          date.today().strftime('2020-09-02'))
     )
+
     assert len([result for result in results]) == 4
--- tests/test_heart_rate.py
+++ tests/test_heart_rate.py
@@ -5,23 +5,14 @@
 import datetime
 from decimal import Decimal
 
-from fitnick.base.base import create_db_engine, get_authorized_client
-from fitnick.heart_rate.heart_rate import get_heart_rate_zone_time_series
-from fitnick.heart_rate.models import heart_daily_table
+from fitnick.base.base import create_db_engine
+from fitnick.heart_rate.heart_rate import insert_heart_rate_time_series_data
 
-HEART_DATERANGE_EXPECTED_ROWS = [
-    ('Out of Range', datetime.date(2020, 8, 20), datetime.date(2020, 8, 27), Decimal('1299.00000'),
-     Decimal('2164.34791')),
-    ('Fat Burn', datetime.date(2020, 8, 20), datetime.date(2020, 8, 27), Decimal('126.00000'), Decimal('819.35015')),
-    ('Cardio', datetime.date(2020, 8, 20), datetime.date(2020, 8, 27), Decimal('2.00000'), Decimal('21.40238')),
-    ('Peak', datetime.date(2020, 8, 20), datetime.date(2020, 8, 27), Decimal('0.00000'), Decimal('0.00000'))
-]
-
-HEART_PERIOD_EXPECTED_ROWS = [
-    ('Out of Range', Decimal('1297.00000'), datetime.date(2020, 8, 26), Decimal('2444.62820')),
-    ('Fat Burn', Decimal('110.00000'), datetime.date(2020, 8, 26), Decimal('814.44840')),
-    ('Cardio', Decimal('0.00000'), datetime.date(2020, 8, 26), Decimal('0.00000')),
-    ('Peak', Decimal('0.00000'), datetime.date(2020, 8, 26), Decimal('0.00000'))
+EXPECTED_ROWS = [
+    ('Out of Range', Decimal('1297.00000'), datetime.date(2020, 8, 26), Decimal('2444.62820'), 65),
+    ('Fat Burn', Decimal('110.00000'), datetime.date(2020, 8, 26), Decimal('814.44840'), 65),
+    ('Cardio', Decimal('0.00000'), datetime.date(2020, 8, 26), Decimal('0.00000'), 65),
+    ('Peak', Decimal('0.00000'), datetime.date(2020, 8, 26), Decimal('0.00000'), 65)
 ]
 
 
@@ -38,21 +29,16 @@ def purge(db_connection, delete_sql_string, select_sql_string):
 
 
 def test_get_heart_rate_time_series_period():
-    db_connection = create_db_engine(database='fitbit_test')
-    authorized_client = get_authorized_client()
+    db_connection = create_db_engine(database='fitbit_test', schema='heart')
 
-    period_data = get_heart_rate_zone_time_series(
-        authorized_client,
-        config={'base_date': '2020-09-02',
-                'period': '1d'},
-        engine=db_connection
-    )
+    purge(db_connection, 'delete from daily;', 'select * from daily')
 
-    base_and_end_date_data = get_heart_rate_zone_time_series(
-        authorized_client,
-        engine=db_connection,
-        config={'base_date': '2020-09-02',
-                'end_date': '2020-09-02'},
-    )
+    insert_heart_rate_time_series_data(config={
+        'database': 'fitbit_test',
+        'base_date': '2020-08-26',
+        'period': '1d'
+    })
 
-    assert period_data == base_and_end_date_data  # asserting that we get the same response from both invocations
+    rows = [row for row in db_connection.execute('select * from daily')]
+
+    assert rows == EXPECTED_ROWS
