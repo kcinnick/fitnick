3fe2a7e97f864e65501bacc2ad6560a5d5b6dcda
unknown <nicktucker4@gmail.com>
2020-09-05 23:53:40 -0400

    fixed database test

--- tests/test_database.py
+++ tests/test_database.py
@@ -10,13 +10,13 @@ from fitnick.heart_rate.models import heart_daily_table
 def test_compare_1d_heart_rate_zone_data():
     db_connection = create_db_engine(database='fitbit_test')
     db_connection.execute(
-        heart_daily_table.delete().where(heart_daily_table.columns.date == date.today().strftime('2020-09-02')))
+        heart_daily_table.delete().where(heart_daily_table.columns.date == date.today().strftime('%Y-%m-%d')))
 
     get_heart_rate_zone_time_series(
         authorized_client=get_authorized_client(),
         engine=db_connection,
         config={
-            'base_date': '2020-09-02',
+            'base_date': date.today().strftime('%Y-%m-%d'),
             'period': '1d'
         }
     )
