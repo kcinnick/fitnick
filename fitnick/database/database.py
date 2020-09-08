9e321a282a8f53e1bc7d72bc3f05e02599c937cb
unknown <nicktucker4@gmail.com>
2020-09-08 00:20:18 -0400

    removed unnecessary function, updated docstring

--- fitnick/database/database.py
+++ fitnick/database/database.py
@@ -2,24 +2,14 @@ import os
 from datetime import timedelta
 from datetime import date
 
-from fitnick.base.base import create_db_engine, create_spark_session
-from fitnick.heart_rate.models import heart_daily_table
-
-
-def build_sql_expression(table, conditions):
-    expression = table.select().where(
-        table.columns.date == conditions[0]).where(
-        table.columns.type == conditions[1]
-    )
-    return expression
-
 
 def compare_1d_heart_rate_zone_data(spark_session, heart_rate_zone='Cardio', database='fitbit', schema="heart"):
     """
     Retrieves & compares today & yesterday's heart rate zone data for the zone specified.
+    :param spark_session: An initialized SparkSession.
     :param heart_rate_zone: str, Heart rate zone data desired. Options are Cardio, Peak, Fat Burn & Out of Range.
     :param database: str, Database to use for data comparison. Options are fitbit or fitbit-test.
-    :param table: sqlalchemy.Table object to retrieve data from.
+    :param schema: Database schema to target.
     :return:
     """
     properties = {
