01ab1056292f9142fadb098bea77517f7687c782
unknown <nicktucker4@gmail.com>
2020-09-09 16:51:54 -0400

    added model to test imports for use in tests

--- tests/test_heart_rate.py
+++ tests/test_heart_rate.py
@@ -10,6 +10,8 @@ from fitnick.base.base import create_db_engine, get_authorized_client
 from fitnick.heart_rate.heart_rate import insert_heart_rate_time_series_data, query_heart_rate_zone_time_series, \
     parse_response
 
+from fitnick.heart_rate.models import heart_daily_table
+
 EXPECTED_ROWS = [
     ('Out of Range', Decimal('1267.00000'), datetime.date(2020, 9, 5), Decimal('2086.83184'), 68),
     ('Fat Burn', Decimal('115.00000'), datetime.date(2020, 9, 5), Decimal('721.58848'), 68),
@@ -41,7 +43,7 @@ def purge(db_connection, delete_sql_string, select_sql_string):
 def test_get_heart_rate_time_series_period():
     db_connection = create_db_engine(database='fitbit_test', schema='heart')
 
-    purge(db_connection, 'delete from heart.daily;', 'select * from heart.daily')
+    db_connection.execute(heart_daily_table.delete())
 
     insert_heart_rate_time_series_data(config={
         'database': 'fitbit_test',
