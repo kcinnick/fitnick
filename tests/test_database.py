e34de3f4da595627644cc6371a3eb0fea5729fc5
unknown <nicktucker4@gmail.com>
2020-09-09 14:34:35 -0400

    fixed DB tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -2,23 +2,15 @@ import decimal
 import os
 from datetime import date, timedelta
 
-import pytest
-
 from fitnick.base.base import create_spark_session, create_db_engine, get_df_from_db
 from fitnick.database.database import compare_1d_heart_rate_zone_data
 from fitnick.heart_rate.heart_rate import insert_heart_rate_time_series_data
 from fitnick.heart_rate.models import heart_daily_table
 
 
-#@pytest.mark.skipif(os.getenv("TEST_LEVEL") != "local", reason='Travis-CI issues')
 def test_compare_1d_heart_rate_zone_data():
-    db_connection = create_db_engine(database='fitbit_test')
-
     today = date.today()
     yesterday = today - timedelta(days=1)
-    db_connection.execute(
-        heart_daily_table.delete().where(heart_daily_table.columns.date == today.strftime('%Y-%m-%d')))
-
     insert_heart_rate_time_series_data(
         config={
             'base_date': today.strftime('%Y-%m-%d'),
@@ -33,7 +25,6 @@ def test_compare_1d_heart_rate_zone_data():
         'Cardio', decimal.Decimal, decimal.Decimal)
 
 
-#@pytest.mark.skipif(os.getenv("TEST_LEVEL") != "local", reason='Travis-CI issues')
 def test_check_for_duplicates():
     """
     Useful for asserting that there aren't duplicates in the database, which *should* be avoided in the code.
