7d87b459b0d5c138258a54aff7649d17c245e656
unknown <nicktucker4@gmail.com>
2020-09-10 15:56:54 -0400

    Reworked tests concerning heart rate zone data stuff to match those model's changes.

--- tests/test_database.py
+++ tests/test_database.py
@@ -7,14 +7,14 @@ from sqlalchemy.exc import IntegrityError
 
 from fitnick.base.base import create_db_engine
 from fitnick.database.database import compare_1d_heart_rate_zone_data
-from fitnick.heart_rate.heart_rate import insert_heart_rate_time_series_data, get_heart_rate_zone_for_day
+from fitnick.heart_rate.heart_rate import HeartRateZone
 from fitnick.heart_rate.models import heart_daily_table
 
 
 @pytest.mark.skipif(os.getenv("TEST_LEVEL") != "local", reason='Travis-CI issues')
 def test_compare_1d_heart_rate_zone_data():
     try:
-        get_heart_rate_zone_for_day(database='fitbit_test', date='2020-09-04')
+        HeartRateZone(config={}).get_heart_rate_zone_for_day(database='fitbit_test', target_date='2020-09-04')
     except IntegrityError:
         # for the purpose of this test, we don't care if the data is already there for 2020-09-04
         pass
@@ -33,12 +33,12 @@ def test_check_for_duplicates():
     """
     db_connection = create_db_engine(database='fitbit_test')
 
-    insert_heart_rate_time_series_data(
-        config={
-            'base_date': date.today().strftime('2020-09-02'),
-            'period': '1d',
-            'database': 'fitbit_test'}
-    )
+    HeartRateZone(config={
+        'base_date': date.today().strftime('2020-09-02'),
+        'period': '1d',
+        'database': 'fitbit_test'}
+    ).insert_heart_rate_time_series_data(raise_exception=False)
+    # For the purpose of this test, we're not concerned with duplicates.
 
     results = db_connection.execute(
         heart_daily_table.select().where(heart_daily_table.columns.date ==
