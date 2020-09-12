137c53a6070c4440eadda56408222c87c24e69ac
unknown <nicktucker4@gmail.com>
2020-09-12 14:22:47 -0400

    Class-ified database functions into a more coherent Database class

--- fitnick/database/database.py
+++ fitnick/database/database.py
@@ -1,6 +1,10 @@
-from datetime import timedelta, datetime, date
+from datetime import timedelta, datetime
+import os
 
-from fitnick.base.base import create_db_engine
+from pyspark.sql import SparkSession
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
+
 from fitnick.heart_rate.models import heart_daily_table
 
 
@@ -12,43 +16,91 @@ def build_sql_expression(table, conditions):
     return expression
 
 
-def compare_1d_heart_rate_zone_data(heart_rate_zone, date_str, database, table=heart_daily_table):
-    """
-    Retrieves & compares today & yesterday's heart rate zone data for the zone specified.
-    :param heart_rate_zone: str, Heart rate zone data desired. Options are Cardio, Peak, Fat Burn & Out of Range.
-    :param database: str, Database to use for data comparison. Options are fitbit or fitbit-test.
-    :param table: sqlalchemy.Table object to retrieve data from.
-    :return:
-    """
-    db_connection = create_db_engine(database=database)
+class Database:
+    def __init__(self, database: str):
+        self.engine = create_engine(
+            f"postgresql+psycopg2://{os.environ['POSTGRES_USERNAME']}:" +
+            f"{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_IP']}" +
+            f":5432/{database}", poolclass=NullPool
+        )
+        self.spark_session = None
 
-    search_datetime = datetime.strptime(date_str, '%Y-%m-%d')
-    previous_date_string = (search_datetime - timedelta(days=1)).date()
+        return
 
-    with db_connection.connect() as connection:
-        day_row = connection.execute(
-            table.select().where(table.columns.date == str(search_datetime.date())
-                                 ).where(table.columns.type == heart_rate_zone)
-        ).fetchone()
+    def create_spark_session(self):
+        """
+        We may not always want a spark session when interacting with the database - i.e.,
+        simple inserts do not require one. Larger scale analysis does, however, and those
+        types of situations are where this method would be called.
+        :return:
+        """
+        if self.spark_session:
+            return
 
-        previous_day_row = connection.execute(
-            table.select().where(table.columns.date == str(previous_date_string)
-                                 ).where(table.columns.type == heart_rate_zone)
-        ).fetchone()
+        self.spark_session = SparkSession.builder.getOrCreate()
+        return
 
-    print(
-        f"You spent {day_row.minutes} minutes in {heart_rate_zone} today, compared to " +
-        f"{previous_day_row.minutes} yesterday."
-    )
+    def get_df_from_db(self, database, schema, table):
+        """
+        Retrieves a PySpark dataframe containing all of the data in the specified table.
+        :param spark_session: Existing SparkSession object
+        :param database: str, name of database
+        :param schema: str, name of schema
+        :param table: str, name of table
+        :return: DataFrame
+        """
+        properties = {
+            "driver": "org.postgresql.Driver",
+            "user": os.environ['POSTGRES_USERNAME'],
+            "password": os.environ['POSTGRES_PASSWORD'],
+            "currentSchema": schema
+        }
 
-    if heart_rate_zone != 'Out of Range':
-        if day_row.minutes < previous_day_row.minutes:
-            print(
-                f'Get moving! That\'s {previous_day_row.minutes - day_row.minutes} minutes less than yesterday!'
-            )
-        else:
-            print('Good work! That\'s {} minutes more than yesterday!'.format(
-                int(day_row.minutes - previous_day_row.minutes)
-            ))
+        df = self.spark_session.read.jdbc(
+            url=f"jdbc:postgresql://{os.environ['POSTGRES_IP']}/{database}",
+            properties=properties,
+            table=table
+        )
 
-    return heart_rate_zone, day_row.minutes, previous_day_row.minutes
+        return df
+
+    def compare_1d_heart_rate_zone_data(self, heart_rate_zone, date_str, database, table=heart_daily_table):
+        """
+        Retrieves & compares today & yesterday's heart rate zone data for the zone specified.
+        :param heart_rate_zone: str, Heart rate zone data desired. Options are Cardio, Peak, Fat Burn & Out of Range.
+        :param date_str: str, representing the date to search for.
+        :param database: str, Database to use for data comparison. Options are fitbit or fitbit-test.
+        :param table: sqlalchemy.Table object to retrieve data from.
+        :return:
+        """
+        search_datetime = datetime.strptime(date_str, '%Y-%m-%d')
+        previous_date_string = (search_datetime - timedelta(days=1)).date()
+
+        with self.engine.connect() as connection:
+            day_row = connection.execute(
+                table.select().where(
+                    table.columns.date == str(search_datetime.date())
+                ).where(table.columns.type == heart_rate_zone)
+            ).fetchone()
+
+            previous_day_row = connection.execute(
+                table.select().where(table.columns.date == str(previous_date_string)
+                                     ).where(table.columns.type == heart_rate_zone)
+            ).fetchone()
+
+        print(
+            f"You spent {day_row.minutes} minutes in {heart_rate_zone} today, compared to " +
+            f"{previous_day_row.minutes} yesterday."
+        )
+
+        if heart_rate_zone != 'Out of Range':
+            if day_row.minutes < previous_day_row.minutes:
+                print(
+                    f'Get moving! That\'s {previous_day_row.minutes - day_row.minutes} minutes less than yesterday!'
+                )
+            else:
+                print('Good work! That\'s {} minutes more than yesterday!'.format(
+                    int(day_row.minutes - previous_day_row.minutes)
+                ))
+
+        return heart_rate_zone, day_row.minutes, previous_day_row.minutes
