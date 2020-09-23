49984c0fe8bc0d05d5c2226871f6f35e61c12370
unknown <nicktucker4@gmail.com>
2020-09-23 00:29:22 -0400

    Updated imports

--- tests/test_database.py
+++ tests/test_database.py
@@ -5,7 +5,7 @@ import pytest
 from sqlalchemy.exc import IntegrityError
 
 from fitnick.database.database import Database
-from fitnick.heart_rate.heart_rate import HeartRateZone
+from fitnick.heart_rate.heart_rate import HeartRateTimeSeries
 from fitnick.heart_rate.models import heart_daily_table
 
 
@@ -13,7 +13,7 @@ from fitnick.heart_rate.models import heart_daily_table
 def test_compare_1d_heart_rate_zone_data():
     database = Database('fitbit_test', 'heart')
     try:
-        HeartRateZone(config={}).get_heart_rate_zone_for_day(database='fitbit_test', target_date='2020-09-04')
+        HeartRateTimeSeries(config={}).get_heart_rate_zone_for_day(database='fitbit_test', target_date='2020-09-04')
     except IntegrityError:
         # for the purpose of this test, we don't care if the data is already there for 2020-09-04
         pass
@@ -33,11 +33,11 @@ def test_check_for_duplicates():
     database = Database('fitbit_test', 'heart')
     connection = database.engine.connect()
 
-    HeartRateZone(config={
+    HeartRateTimeSeries(config={
         'base_date': '2020-09-02',
         'period': '1d',
         'database': 'fitbit_test'}
-    ).insert_heart_rate_time_series_data()
+    ).insert_data()
 
     results = connection.execute(
         heart_daily_table.select().where(heart_daily_table.columns.date == '2020-09-02'))
