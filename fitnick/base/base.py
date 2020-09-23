1089325e85378926e372b65555072cb43e6c6ae4
unknown <nicktucker4@gmail.com>
2020-09-23 00:29:22 -0400

    put a new TimeSeries class in base.py, to handle all of the time series methods instead of rewriting the same methods for all of them!

--- fitnick/base/base.py
+++ fitnick/base/base.py
@@ -1,10 +1,43 @@
 """Main module."""
 import os
+from datetime import datetime, timedelta, date
 
 import fitbit
 from pyspark.sql import SparkSession
 from sqlalchemy import create_engine
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import FlushError
+from tqdm import tqdm
+
+from fitnick.database.database import Database
+from sqlalchemy.dialects.postgresql import insert
+from fitnick.heart_rate.models import heart_daily_table
+
+
+def handle_integrity_error(session, row):
+    session.rollback()
+    insert_statement = insert(heart_daily_table).values(
+        type=row.type,
+        minutes=row.minutes,
+        date=row.date,
+        calories=row.calories,
+        resting_heart_rate=row.resting_heart_rate)
+
+    update_statement = insert_statement.on_conflict_do_update(
+        constraint='daily_type_date_key',
+        set_={
+            'type': row.type,
+            'minutes': row.minutes,
+            'date': row.date,
+            'calories': row.calories,
+            'resting_heart_rate': row.resting_heart_rate
+        })
+
+    session.execute(update_statement)
+    session.commit()
+
+    return session
 
 
 def get_authorized_client() -> fitbit.Fitbit:
@@ -89,3 +122,94 @@ def create_spark_session():
     spark = SparkSession.builder.getOrCreate()
 
     return spark
+
+
+class TimeSeries:
+    """
+    Contains common methods used when accessing time-series-based data,
+    like heart rate, sleep, activity, etc. This class isn't intended to
+    be used on it's own but serve as a base class for endpoint-specific
+    classes.
+    """
+    def __init__(self, config):
+        self.config = config
+        self.authorized_client = get_authorized_client()
+        return
+
+    def query(self):
+        """
+        The two time-series based queries supported are documented here:
+        https://dev.fitbit.com/build/reference/web-api/heart-rate/#get-heart-rate-time-series
+        :return:
+        """
+        try:
+            assert len(self.config['base_date'].split('-')[0]) == 4
+        except AssertionError:
+            print('Dates must be formatted as YYYY-MM-DD. Exiting.')
+            exit()
+
+        base_date = datetime.strptime(self.config['base_date'], '%Y-%m-%d')
+        period = self.config.get('period')
+
+        if period:
+            if period in ['1m', '30d']:
+                self.config['end_date'] = base_date + timedelta(days=30)
+            elif period in ['7d', '1w']:
+                self.config['end_date'] = base_date + timedelta(days=7)
+            elif period == '1d':
+                self.config['end_date'] = base_date + timedelta(days=1)
+            else:
+                raise NotImplementedError(f'Period {period} is not supported.\n')
+
+        data = self.authorized_client.time_series(
+            resource=f'activities/{self.config["resource"]}',
+            base_date=self.config['base_date'],
+            end_date=self.config['end_date']
+        )
+
+        return data
+
+    def insert_data(self):
+        """
+        Extracts, transforms & loads the data specified by the self.config dict.
+        :return:
+        """
+
+        data = self.query()
+        parsed_rows = self.parse_response(data)
+        db = Database(self.config['database'], schema=self.config['schema'])
+
+        # create a session connected to the database in config
+        session = sessionmaker(bind=db.engine)()
+
+        for row in tqdm(parsed_rows):
+            try:
+                session.add(row)
+                session.commit()
+            except FlushError:
+                session.expunge_all()
+                session.rollback()
+                session.add(row)
+                try:
+                    session.commit()
+                except IntegrityError:
+                    session = handle_integrity_error(session=session, row=row)
+                    continue
+            except IntegrityError:
+                session = handle_integrity_error(session, row)
+
+        session.close()
+
+        return parsed_rows
+
+    def backfill(self, period: int = 90):
+        """
+        Backfills a database from the current day.
+        Example: if run on 2020-09-06 with period=90, the database will populate for 2020-06-08 - 2020-09-06
+        :param period: Number of days to look backward.
+        :return:
+        """
+        self.config['base_date'] = (date.today() - timedelta(days=period)).strftime('%Y-%m-%d')
+        self.config['end_date'] = date.today().strftime('%Y-%m-%d')
+
+        self.insert_data()
