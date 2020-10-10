fa435b565d06fff3486223f3723672117c6bb3c3
unknown <nicktucker4@gmail.com>
2020-10-10 15:33:12 -0400

    updated test for get_body_fat

--- tests/test_body.py
+++ tests/test_body.py
@@ -11,6 +11,17 @@ EXPECTED_WEIGHT_RESPONSE = {'body-weight': [{'dateTime': '2020-09-05', 'value': 
 
 EXPECTED_WEIGHT_DATA = [(datetime.date(2020, 9, 5), Decimal('176.0')), (datetime.date(2020, 9, 6), Decimal('175.2'))]
 
+EXPECTED_BODYFAT_RESPONSE = {
+    'fat': [
+        {'date': '2020-09-05', 'fat': 18, 'logId': 1599350399000, 'source': 'API', 'time': '23:59:59'},
+        {'date': '2020-09-11', 'fat': 18, 'logId': 1599868799000, 'source': 'API', 'time': '23:59:59'},
+        {'date': '2020-09-18', 'fat': 18, 'logId': 1600473599000, 'source': 'API', 'time': '23:59:59'},
+        {'date': '2020-09-22', 'fat': 18, 'logId': 1600819199000, 'source': 'API', 'time': '23:59:59'},
+        {'date': '2020-09-23', 'fat': 18, 'logId': 1600905599000, 'source': 'API', 'time': '23:59:59'},
+        {'date': '2020-09-27', 'fat': 18, 'logId': 1601251199000, 'source': 'API', 'time': '23:59:59'},
+        {'date': '2020-09-30', 'fat': 18, 'logId': 1601510399000, 'source': 'API', 'time': '23:59:59'}]
+}
+
 
 def test_query_body_weight_time_series():
     data = WeightTimeSeries(config={
@@ -63,10 +74,10 @@ def test_body_plot():
 
 
 def test_get_body_fat():
-    logs = BodyFat(config={
+    response = BodyFat(config={
         'database': 'fitbit_test',
         'base_date': '2020-09-05',
         'period': '1m',
         'table': 'daily'}).query()
 
-    assert len(logs['fat']) == 7
+    assert response == EXPECTED_BODYFAT_RESPONSE
