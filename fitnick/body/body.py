86fa3a21c3c6a8b3a6c2e3d917a1ac802d5bc5dd
unknown <nicktucker4@gmail.com>
2020-10-13 13:39:51 -0400

    Added insert method for bodyfat data

--- fitnick/body/body.py
+++ fitnick/body/body.py
@@ -1,3 +1,6 @@
+from sqlalchemy.exc import IntegrityError
+from tqdm import tqdm
+
 from fitnick.base.base import get_authorized_client
 from sqlalchemy.orm import sessionmaker
 
@@ -46,7 +49,7 @@ class WeightTimeSeries(TimeSeries):
 
 class BodyFat:
     def __init__(self, config):
-        self.config = {'schema': 'weight', 'resource': 'fat'}
+        self.config = {'schema': 'bodyfat', 'resource': 'fat'}
         self.config.update(config)
         self.authorized_client = get_authorized_client()
 
@@ -58,7 +61,7 @@ class BodyFat:
         #  unlike the 'steps' or 'heart-rate' kind of activities, there are only
         #  entries for body fat if the authorized user proactively creates them.
         #  because of this, some unexpected behavior may occur - i.e. querying 1m
-        #  will only return as many entries as there were in that month, not 31.
+        #  will only return as many entries as there were in that month, not 30/31.
 
         response = self.authorized_client.make_request(
             method='get',
@@ -81,3 +84,29 @@ class BodyFat:
             rows.append(row)
 
         return rows
+
+    def insert_data(self):
+        """
+        Extracts, transforms & loads the data specified by the self.config dict.
+        :return:
+        """
+
+        data = self.query()
+        parsed_rows = self.parse_response(data)
+        db = Database(self.config['database'], schema=self.config['schema'])
+        session = sessionmaker(bind=db.engine)()
+        for row in parsed_rows:
+            insert_statement = insert(bodyfat_table).values(
+                date=row.date,
+                fat=row.fat,
+                logid=row.logId,
+                source=row.source,
+                time=row.time
+            )
+            try:
+                session.execute(insert_statement)
+                session.commit()
+            except IntegrityError:  # record already exists
+                pass
+
+        session.close()
