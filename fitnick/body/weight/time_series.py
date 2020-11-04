d76f21006a6830c057e786b9e8377f123016d7b8
unknown <nicktucker4@gmail.com>
2020-11-04 16:11:57 -0500

    Removed insert_data method, moved it to base TimeSeries class.

--- fitnick/body/weight/time_series.py
+++ fitnick/body/weight/time_series.py
@@ -1,8 +1,4 @@
-from sqlalchemy.dialects.postgresql import insert
-from sqlalchemy.orm import sessionmaker
-
-from fitnick.body.models.weight import WeightRecord, weight_table
-from fitnick.database.database import Database
+from fitnick.body.models.weight import WeightRecord
 from fitnick.time_series import TimeSeries
 
 
@@ -23,19 +19,3 @@ class WeightTimeSeries(TimeSeries):
             rows.append(row)
 
         return rows
-
-    def insert_data(self):
-        data = self.query()
-        parsed_rows = self.parse_response(data)
-        db = Database(self.config['database'], schema=self.config['schema'])
-        session = sessionmaker(bind=db.engine)()
-        for row in parsed_rows:
-            insert_statement = insert(weight_table).values(
-                date=row.date,
-                pounds=row.pounds
-            )
-            session.execute(insert_statement)
-            session.commit()
-
-        session.close()
-        return
