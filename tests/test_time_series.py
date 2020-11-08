8e86c7c265b219ecc5fcef3f89e45e9175751451
unknown <nicktucker4@gmail.com>
2020-11-08 00:27:44 -0500

    added test for backfill

--- tests/test_time_series.py
+++ tests/test_time_series.py
@@ -1,5 +1,5 @@
 from fitnick.time_series import TimeSeries
-
+from fitnick.activity.models.calories import calories_table
 import pytest
 
 
@@ -15,3 +15,15 @@ def test_validate_inputs():
             config={'base_date': '10-1-2020',
                     'end_date': '10-02-2020'}
         ).validate_input()
+
+
+def test_backfill():
+    time_series = TimeSeries(
+        config={'base_date': '2020-10-01',
+                'end_date': '2020-10-02',
+                'database': 'fitbit_test',
+                'schema': 'activity',
+                'table': calories_table,
+                'resource': 'calories'})
+
+    time_series.backfill(90)
