4282eb717fd2213bebfd3fd0493673e3fdba6908
unknown <nicktucker4@gmail.com>
2020-12-25 14:45:32 -0500

    updated tests to match new fitbit (!!!)

--- tests/test_activity.py
+++ tests/test_activity.py
@@ -8,44 +8,8 @@ from fitnick.activity.models.calories import Calories, calories_table
 from fitnick.database.database import Database
 from fitnick.time_series import plot_rolling_average
 
-EXPECTED_DAILY_ACTIVITY_RESPONSE = {
-    'activities': [
-        {'activityId': 20049, 'activityParentId': 20049, 'activityParentName': 'Treadmill', 'calories': 170, 'description': '', 'distance': 0.987632, 'duration': 1233000, 'hasActiveZoneMinutes': False, 'hasStartTime': True, 'isFavorite': False, 'lastModified': '2020-10-01T04:31:23.000Z', 'logId': 34686180447, 'name': 'Treadmill', 'startDate': '2020-10-01', 'startTime': '00:10', 'steps': 2030},
-        {'activityId': 20049, 'activityParentId': 20049, 'activityParentName': 'Treadmill', 'calories': 99, 'description': '', 'distance': 0.577689, 'duration': 767000, 'hasActiveZoneMinutes': False, 'hasStartTime': True, 'isFavorite': False, 'lastModified': '2020-10-01T18:10:22.000Z', 'logId': 34685023369, 'name': 'Treadmill', 'startDate': '2020-10-01', 'startTime': '13:55', 'steps': 1218},
-        {'activityId': 20049, 'activityParentId': 20049, 'activityParentName': 'Treadmill', 'calories': 59, 'description': '', 'distance': 0.040327, 'duration': 2124000, 'hasActiveZoneMinutes': False, 'hasStartTime': True, 'isFavorite': False, 'lastModified': '2020-10-01T21:21:59.000Z', 'logId': 34687543940, 'name': 'Treadmill', 'startDate': '2020-10-01', 'startTime': '16:22', 'steps': 114},
-        {'activityId': 20049, 'activityParentId': 20049, 'activityParentName': 'Treadmill', 'calories': 104, 'description': '', 'distance': 0.440571, 'duration': 815000, 'hasActiveZoneMinutes': False, 'hasStartTime': True, 'isFavorite': False, 'lastModified': '2020-10-01T23:15:56.000Z', 'logId': 34684730042, 'name': 'Treadmill', 'startDate': '2020-10-01', 'startTime': '19:01', 'steps': 977},
-        {'activityId': 20049, 'activityParentId': 20049, 'activityParentName': 'Treadmill', 'calories': 481, 'description': '', 'distance': 2.876452, 'duration': 3402000, 'hasActiveZoneMinutes': False, 'hasStartTime': True, 'isFavorite': False, 'lastModified': '2020-10-02T03:07:46.000Z', 'logId': 34687447116, 'name': 'Treadmill', 'startDate': '2020-10-01', 'startTime': '22:10', 'steps': 6012}],
-    'goals': {'activeMinutes': 70, 'caloriesOut': 3000, 'distance': 8.05, 'floors': 10, 'steps': 11000},
-    'summary': {'activeScore': -1, 'activityCalories': 1467, 'caloriesBMR': 1838, 'caloriesOut': 3116, 'distances': [
-        {'activity': 'Treadmill', 'distance': 0.987632},
-        {'activity': 'Treadmill', 'distance': 0.577689},
-        {'activity': 'Treadmill', 'distance': 0.040327},
-        {'activity': 'Treadmill', 'distance': 0.440571},
-        {'activity': 'Treadmill', 'distance': 2.876452},
-        {'activity': 'total', 'distance': 6.08},
-        {'activity': 'tracker', 'distance': 6.08},
-        {'activity': 'loggedActivities', 'distance': 4.922670999999999},
-        {'activity': 'veryActive', 'distance': 4.1},
-        {'activity': 'moderatelyActive', 'distance': 0.77},
-        {'activity': 'lightlyActive', 'distance': 1.2},
-        {'activity': 'sedentaryActive', 'distance': 0}
-    ],
-        'elevation': 70,
-        'fairlyActiveMinutes': 22,
-        'floors': 7,
-        'heartRateZones': [
-            {'caloriesOut': 2203.32886, 'max': 96, 'min': 30, 'minutes': 1297, 'name': 'Out of Range'},
-            {'caloriesOut': 788.48992, 'max': 134, 'min': 96, 'minutes': 104, 'name': 'Fat Burn'},
-            {'caloriesOut': 80.4321, 'max': 163, 'min': 134, 'minutes': 7, 'name': 'Cardio'},
-            {'caloriesOut': 0, 'max': 220, 'min': 163, 'minutes': 0, 'name': 'Peak'}
-        ],
-        'lightlyActiveMinutes': 155,
-        'marginalCalories': 922,
-        'restingHeartRate': 62,
-        'sedentaryMinutes': 680,
-        'steps': 12738,
-        'veryActiveMinutes': 82}
-}
+EXPECTED_DAILY_ACTIVITY_RESPONSE = {'activities': [{'activityId': 20049, 'activityParentId': 20049, 'activityParentName': 'Treadmill', 'calories': 91, 'description': '', 'distance': 0.577838, 'duration': 679000, 'hasActiveZoneMinutes': False, 'hasStartTime': True, 'isFavorite': False, 'lastModified': '2020-12-24T16:01:58.000Z', 'logId': 36487726513, 'name': 'Treadmill', 'startDate': '2020-12-24', 'startTime': '10:44', 'steps': 1236}, {'activityId': 20049, 'activityParentId': 20049, 'activityParentName': 'Treadmill', 'calories': 184, 'description': '', 'distance': 1.178393, 'duration': 1552000, 'hasActiveZoneMinutes': False, 'hasStartTime': True, 'isFavorite': False, 'lastModified': '2020-12-24T17:03:52.000Z', 'logId': 36487795727, 'name': 'Treadmill', 'startDate': '2020-12-24', 'startTime': '11:37', 'steps': 2524}, {'activityId': 20049, 'activityParentId': 20049, 'activityParentName': 'Treadmill', 'calories': 77, 'description': '', 'distance': 0.492319, 'duration': 608000, 'hasActiveZoneMinutes': False, 'hasStartTime': True, 'isFavorite': False, 'lastModified': '2020-12-24T19:19:52.000Z', 'logId': 36489026309, 'name': 'Treadmill', 'startDate': '2020-12-24', 'startTime': '13:50', 'steps': 1035}, {'activityId': 20049, 'activityParentId': 20049, 'activityParentName': 'Treadmill', 'calories': 126, 'description': '', 'distance': 0.754637, 'duration': 1099000, 'hasActiveZoneMinutes': False, 'hasStartTime': True, 'isFavorite': False, 'lastModified': '2020-12-25T00:32:21.000Z', 'logId': 36489666573, 'name': 'Treadmill', 'startDate': '2020-12-24', 'startTime': '19:01', 'steps': 1618}, {'activityId': 20049, 'activityParentId': 20049, 'activityParentName': 'Treadmill', 'calories': 255, 'description': '', 'distance': 1.671799, 'duration': 1969000, 'hasActiveZoneMinutes': False, 'hasStartTime': True, 'isFavorite': False, 'lastModified': '2020-12-25T04:01:25.000Z', 'logId': 36489745633, 'name': 'Treadmill', 'startDate': '2020-12-24', 'startTime': '22:17', 'steps': 3502}], 'goals': {'activeMinutes': 70, 'caloriesOut': 3100, 'distance': 9.66, 'floors': 10, 'steps': 12000}, 'summary': {'activeScore': -1, 'activityCalories': 1206, 'caloriesBMR': 1808, 'caloriesOut': 2867, 'distances': [{'activity': 'Treadmill', 'distance': 0.577838}, {'activity': 'Treadmill', 'distance': 1.178393}, {'activity': 'Treadmill', 'distance': 0.492319}, {'activity': 'Treadmill', 'distance': 0.754637}, {'activity': 'Treadmill', 'distance': 1.671799}, {'activity': 'total', 'distance': 5.73}, {'activity': 'tracker', 'distance': 5.73}, {'activity': 'loggedActivities', 'distance': 4.6749860000000005}, {'activity': 'veryActive', 'distance': 3.58}, {'activity': 'moderatelyActive', 'distance': 0.76}, {'activity': 'lightlyActive', 'distance': 1.38}, {'activity': 'sedentaryActive', 'distance': 0}], 'elevation': 120, 'fairlyActiveMinutes': 18, 'floors': 12, 'heartRateZones': [{'caloriesOut': 2482.44676, 'max': 96, 'min': 30, 'minutes': 1372, 'name': 'Out of Range'}, {'caloriesOut': 349.55904, 'max': 134, 'min': 96, 'minutes': 46, 'name': 'Fat Burn'}, {'caloriesOut': 0, 'max': 163, 'min': 134, 'minutes': 0, 'name': 'Cardio'}, {'caloriesOut': 0, 'max': 220, 'min': 163, 'minutes': 0, 'name': 'Peak'}], 'lightlyActiveMinutes': 141, 'marginalCalories': 719, 'restingHeartRate': 64, 'sedentaryMinutes': 703, 'steps': 12053, 'veryActiveMinutes': 68}}
+
 
 EXPECTED_DAILY_ACTIVITY_ROWS = [
     ActivityLogRecord(activity_id=20049, activity_name='Treadmill', log_id=34686180447, calories=170, distance=0.987632, duration=1233000, duration_minutes=20.55, start_date='2020-10-01', start_time='00:10', steps=2030),
@@ -61,11 +25,10 @@ EXPECTED_COMPARE_CALORIE_ROWS = []
 def test_query_daily_activity_summary():
     activity = Activity(
         config={'database': 'fitbit_test',
-                'base_date': '2020-10-01'}
+                'base_date': '2020-12-24'}
     )
 
     response = activity.query_daily_activity_summary()
-
     assert response == EXPECTED_DAILY_ACTIVITY_RESPONSE
 
 
