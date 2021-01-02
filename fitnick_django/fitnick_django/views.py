2302c656cf688f0e5ec893468c5b5929ebda927d
unknown <nicktucker4@gmail.com>
2021-01-02 16:33:14 -0500

    update views.index to fill in new template

--- fitnick_django/fitnick_django/views.py
+++ fitnick_django/fitnick_django/views.py
@@ -1,9 +1,24 @@
+from datetime import datetime
+
 from django.shortcuts import render
+from fitnick.activity.activity import Activity
 
 
 def index(request):
+
+    activity_api = Activity(config={'base_date': '2021-01-02'}) # replace with real day checks
+    response = activity_api.query_daily_activity_summary()
+    steps_this_time = response['summary']['steps']
+    dt = datetime.now()
+    print(steps_this_time, dt)
     index_context = {
-        "index": 'index!',
+        "base_date": activity_api.config['base_date'],
+        "steps": steps_this_time,
+        "time": str(dt)
     }
+    #  get steps
+    #  print steps
+    #  eventually, print a progress bar re ccurrent steps/daily goal
+    # iterate from there!
 
     return render(request, 'index.html', index_context)
