e88575bab986466d2aaf7373564f042e332c01f8
unknown <nicktucker4@gmail.com>
2021-01-15 18:19:05 -0500

    views updates

--- fitnick_django/fitnick_django/urls.py
+++ fitnick_django/fitnick_django/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import index
+from .views import index, get_steps_today
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('steps/today', get_steps_today)
 ]
--- fitnick_django/fitnick_django/views.py
+++ fitnick_django/fitnick_django/views.py
@@ -5,7 +5,6 @@ from fitnick.activity.activity import Activity
 
 
 def index(request):
-
     activity_api = Activity(config={'base_date': '2021-01-02'})
     response = activity_api.query_daily_activity_summary()
     steps_this_time = response['summary']['steps']
@@ -16,9 +15,23 @@ def index(request):
         "steps": steps_this_time,
         "time": str(dt)
     }
-    #  get steps
-    #  print steps
-    #  eventually, print a progress bar re ccurrent steps/daily goal
-    # iterate from there!
+
+    return render(request, 'index.html', index_context)
+
+
+def get_steps_today(request):
+    goal = 12000  # set automatically, eventually..
+    today = datetime.today().strftime('%Y-%m-%d')
+    activity_api = Activity(config={'base_date': today})
+    response = activity_api.query_daily_activity_summary()
+    steps_this_time = response['summary']['steps']
+    dt = datetime.now()
+    print(steps_this_time, dt)
+    index_context = {
+        "base_date": activity_api.config['base_date'],
+        "steps": steps_this_time,
+        "time": str(dt),
+        "goal": goal
+    }
 
     return render(request, 'index.html', index_context)
